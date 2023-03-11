801ccd7245a3cba86271626fa0e54879375e8d26
Maurizio Poli <mauriziopoli.02@gmail.com>
2023-03-11 16:06:11 +0100

    Add comments

--- telegrambot.py
+++ telegrambot.py
@@ -2,15 +2,18 @@ import requests
 from dotenv import load_dotenv
 import os
 
-load_dotenv() #Carica variabili fine .env
+load_dotenv() #Load .env variable
 
 def send_msg(text):
     token = os.getenv("TOKEN")
     chat_id = os.getenv("CHAT_ID")
 
     url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
+    
+    #Send the message in telegram
     result = requests.get(url_req)
     
+    #Print on terminal the result
     print(result.json())
 
-send_msg("Messaggio automatico python")
+send_msg("Hello my friend!")
